938cfdb708dfea3bac80798f5e21b2d76094c5ef
VakarisN <vakarisneverauskas@gmail.com>
2018-10-16 12:54:32 +0300

    HTML tekstas

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php print 'Dope website'; ?>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aš valdau HTML</title>
+  </head>
+  <body>
+    <h1>Vakaris - HTML asas!</h1>
+    <p> Moku kurti statinį turinį</p>
+    <?php print 'Dope website'; ?>
+  </body>
+</html>
