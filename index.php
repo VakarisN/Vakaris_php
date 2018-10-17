e8f21565ad1a3c26958df3244434b2e729f55d6b
VakarisN <vakarisneverauskas@gmail.com>
2018-10-17 14:33:02 +0300

    Pinigu skaiciuokle

--- index.php
+++ index.php
@@ -5,16 +5,14 @@
         <meta charset="utf-8">
         <title></title>
          <?php
-$mano_pinigai = 1000;
-$spent_per_month = 600;
-$earned_per_month = 800;
-$unknown_per_month = rand(20, 199);
-$months = rand (2, 24);
-$wallet_forecast = $mano_pinigai + ($earned_per_month - $spent_per_month - $unknown_per_month) * $months;
+$liepa_days = 31;
+$rugpjutis_days = 30;
+$rugsejis_days = 30;
+$dienos_iki_dabar = $liepa_days + $rugpjutis_days + $rugsejis_days + date('d');
 ?>
     </head>
     <body>
-        Po <?php print $months ?> menesiu, tiketina turesiu <?php print $wallet_forecast?> pinigu.
+        Nuo Liepos pradzios iki siandien praejo <?php print $dienos_iki_dabar ?> dienu.
 
 
     </body>
