e4bd96b45ee00f053675938d65b4399227ef5c7e
VakarisN <vakarisneverauskas@gmail.com>
2018-10-18 14:26:38 +0300

    Ar miegosi ant sofos updeitintas

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $grizai_isgeres =rand(0, 1);
 $text =null;
 if ($grizai_velai && $grizai_isgeres){
     $text = 'Miegosi ant sofos, nes grizai velai ir isgeres';
-} elseif (!$grizai_isgeres){
+} elseif ($grizai_isgeres){
 $text = 'Nemiegosi ant sofos, nes negrizai isgeres';
-} elseif (!$grizai_isgeres){
+} elseif ($grizai_velai){
 $text = 'Nemiegosi ant sofos, nes negrizai velai';
 } else {
 $text = 'Nemiegosi ant sofos, nes negrizai nei velai nei isgeres';
