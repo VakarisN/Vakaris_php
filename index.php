69343460458bcbf5e9c273463b44ba679d756272
VakarisN <vakarisneverauskas@gmail.com>
2018-10-18 12:59:09 +0300

    Kuro sanaudu skaiciuokle

--- index.php
+++ index.php
@@ -1,55 +1,20 @@
+<?php
+$distance = rand(100, 10000);
+$fuel_100km = 7.5;
+$fuel_price = 1.32;
+$kuro_kaina = ($distance / 100) * $fuel_100km * $fuel_price;
+$kuro_sanaudos = ($distance / 100) * $fuel_100km;
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-      <style>
-           .debesuota_be_lietaus_be_saules {
-               background-image: url("https://cdn4.iconfinder.com/data/icons/heavy-weather/100/Weather_Icons_02_cloud-512.png");
-               height: 200px;
-               width: 200px;
-               background-size: cover;
-           }
-           .debesuota_su_lietumi_be_saules {
-               background-image: url("https://cdn1.iconfinder.com/data/icons/weather-elements/512/Weather_CloudRain.png");
-               height: 200px;
-               width: 200px;
-               background-size: cover;
-           }
-           .debesuota_su_lietumi_su_saule {
-               background-image: url("https://cdn3.iconfinder.com/data/icons/sunnyday-2/142/light_cloudy_heavy_rain-512.png");
-               height: 200px;
-               width: 200px;
-               background-size: cover;
-           }
-           .tik_sauleta {
-               background-image: url("https://cdn1.iconfinder.com/data/icons/weather-elements/512/Weather_SunAbstract.png");
-               height: 200px;
-               width: 200px;
-               background-size: cover;
-           }
-       </style>
-       <?php
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Nuvaziavus <?php print $distance ?> km, automobilis sunaudos <?php print $kuro_sanaudos ?> L kuro <br>
+            Kaina: <?php print $kuro_kaina ?></h1>
 
-       $sun = rand(0,1);
-       $rain = rand(0,1);
-
-       ?>
-       <?php if ($sun == true): ?>
-           <?php if ($rain == true): ?>
-               <div class="debesuota_su_lietumi_su_saule">Debesuota su lietumi ir su saule</div>
-           <?php elseif ($rain == false): ?>
-               <div class="tik_sauleta">Tik sauleta</div>
-           <?php endif;?>
-       <?php elseif ($sun == false): ?>
-           <?php if ($rain == true): ?>
-               <div class="debesuota_su_lietumi_be_saules">Debesuota su lietumi be saules</div>
-           <?php elseif ($rain == false): ?>
-               <div class="debesuota_be_lietaus_be_saules">Tik debesuota</div>
-           <?php endif;?>
-       <?php endif;?>
-
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-  </body>
+    </body>
 </html>
