b8c115b426d2b4c46f1ec8f2ada72bd0af2ab015
VakarisN <vakarisneverauskas@gmail.com>
2018-10-18 20:42:30 +0300

    $x kartu prie $x prideti $y ciklas baigesi

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 $x = 10;
 $y = 2;
-$kiekis = 10;
+$kiekis = $x;
 $i = null;
-for ($i = 0; $i < $kiekis; $i++) {
+for ($i = 0; $x < 40; $i++) {
     $text = print nl2br("Dabartinė \$x vertė: $x\n");
     $x += $y;
+}
+if ($x>=20) {
+    print 'Ciklas nutrauktas';
 }
 ?>
 <html>
