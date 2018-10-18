c39ec36fa15e33a6f4f9c85280e056d98ed95301
VakarisN <vakarisneverauskas@gmail.com>
2018-10-18 14:23:07 +0300

    Ar miegosi ant sofos

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-$distance = rand(100, 10000);
-$fuel_100km = 7.5;
-$fuel_price = 1.32;
-$mano_pinigai = 200;
-$kuro_kaina = ($distance / 100) * $fuel_100km * $fuel_price;
-$kuro_sanaudos = ($distance / 100) * $fuel_100km;
+$grizai_velai =rand(0, 1);
+$grizai_isgeres =rand(0, 1);  
+$text =null;
+if ($grizai_velai && $grizai_isgeres){
+    $text = 'Miegosi ant sofos, nes grizai velai ir isgeres';
+} elseif (!$grizai_isgeres){
+$text = 'Nemiegosi ant sofos, nes negrizai isgeres';
+} elseif (!$grizai_isgeres){
+$text = 'Nemiegosi ant sofos, nes negrizai velai';
+} else {
+$text = 'Nemiegosi ant sofos, nes negrizai nei velai nei isgeres';
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,14 +20,7 @@ $kuro_sanaudos = ($distance / 100) * $fuel_100km;
         <title></title>
     </head>
     <body>
-        <h1>Nuvaziavus <?php print $distance ?> km, automobilis sunaudos <?php print $kuro_sanaudos ?> L kuro <br>
-            Kaina: <?php print $kuro_kaina ?></h1>
-        <?php if ($kuro_kaina > $mano_pinigai): ?>
-            <h1>Jeigu reiketu vaziuoti <?php print $distance ?> <br>
-                Isvada: As sau  negaliu to  leisti.</h1>
-        <?php else: ?>
-            <h1>Jeigu reiketu vaziuoti <?php print $distance ?> <br>
-                Isvada: As sau  galiu tai  leisti.</h1>
-        <?php endif; ?>
+        <h1><?php print $text ?></h1>
+        
     </body>
 </html>
