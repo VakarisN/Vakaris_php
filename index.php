e2bcd15e4451fee1af53da5607f94b0248b23ced
VakarisN <vakarisneverauskas@gmail.com>
2018-10-18 20:14:06 +0300

    Ciklas antra uzduotis x+y

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
 $x = 10;
-$text = 'Pradedam: ';
-$i =null;
-for ($i = 0; $i < $x; $i++) {
-    $text .= 'Ciklas ';
+$y = 2;
+$kiekis = 10;
+$i = null;
+for ($i = 0; $i < $kiekis; $i++) {
+    $text = print nl2br("Dabartinė \$x vertė: $x\n");
+    $x += $y;
 }
 ?>
 <html>
+    <head>
+        <title>Antra užduotis</title>
+    </head>
     <body>
         <p>
-            <?php print $text; ?>
+            <?php
+            $text;
+            ?>
         </p>
-
     </body>
 </html>
