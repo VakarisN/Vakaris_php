9a504949243fe815d053a61363f73e0b4570cb18
VakarisN <vakarisneverauskas@gmail.com>
2018-10-18 13:19:55 +0300

    Kuro sanaudu skaiciuokle

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $distance = rand(100, 10000);
 $fuel_100km = 7.5;
 $fuel_price = 1.32;
+$mano_pinigai = 200;
 $kuro_kaina = ($distance / 100) * $fuel_100km * $fuel_price;
 $kuro_sanaudos = ($distance / 100) * $fuel_100km;
 ?>
@@ -15,6 +16,12 @@ $kuro_sanaudos = ($distance / 100) * $fuel_100km;
     <body>
         <h1>Nuvaziavus <?php print $distance ?> km, automobilis sunaudos <?php print $kuro_sanaudos ?> L kuro <br>
             Kaina: <?php print $kuro_kaina ?></h1>
-
+        <?php if ($kuro_kaina > $mano_pinigai): ?>
+            <h1>Jeigu reiketu vaziuoti <?php print $distance ?> <br>
+                Isvada: As sau  negaliu to  leisti.</h1>
+        <?php else: ?>
+            <h1>Jeigu reiketu vaziuoti <?php print $distance ?> <br>
+                Isvada: As sau  galiu tai  leisti.</h1>
+        <?php endif; ?>
     </body>
 </html>
