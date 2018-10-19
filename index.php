dd7d95dfcb6df82a0b178a3d1b69a8b8c3a54802
VakarisN <vakarisneverauskas@gmail.com>
2018-10-19 15:00:51 +0300

    Array foreach RANDOM

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 $fridge = ['Jogurtas', 'Kebabas', 'Alus', 'Sugede vaisiai', 'Supuves avokadas'];
-$text =null;
-foreach ($fridge as $produktas){
-  $text .= $produktas. ' '; 
+$text = null;
+foreach ($fridge as $produktas) {
+    $text .= $produktas . ' ';
 }
-
 ?>
 <html>
     <head>
         <title>Foreach</title>
     </head>
-   <body>
-       <div> <?php print $text?> </div>
-   </body>
+    <body>
+        <div> <?php print $fridge[rand(0, 4)]; ?> </div>
+    </body>
 </html>
